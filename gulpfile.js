67838787b81f97c64a42dd2d0924faf357db8aac
Wellington Ramos <wrm.net@gmail.com>
2019-05-28 18:30:30 -0300

    stylizando

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,13 @@ var sass = require('gulp-sass');
 sass.compiler = require('node-sass');
 
 gulp.task('sass', function () {
-    return gulp.src('./src/assets/scss/*.scss')
+    return gulp
+        .src('./src/assets/scss/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./src/assets/css/'));
 });
 
-gulp.task('sass:watch', function () {
-    gulp.watch('./src/assets/scss/*.scss', ['sass']);
+gulp.task('watch', function () {
+    return gulp 
+        .watch('./src/assets/scss/*.scss', gulp.series('sass'));
 });
--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import './assets/css/App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div className="header">
+        
+      </div>
     </div>
   );
 }
