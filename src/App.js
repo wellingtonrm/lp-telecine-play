7a0f3de319115f00a1ebb4188bc5e8e88b313f27
Wellington Ramos <wrm.net@gmail.com>
2019-05-28 21:34:38 -0300

    ✔️ usando bootstrap  e fontawesome

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-//import logo from './assets/svg/logo.svg';
 import './assets/css/App.css';
-
+import './assets/bootstrap/css/bootstrap.css';
+import './assets/fontawesome/css/all.css';
 function App() {
   return (
-    <div className="App">
-      <div className="header">
-        
+    <div id="App">
+      <div className="logo">
       </div>
+      <h2>Filmes exclusivos dos maiores estúdios, é <span>só dar um play</span></h2>
+      <h3>Curta 7 dias grátis, assine por R$37,90/mês e cancele quando quiser</h3>
+      <button type="button" className="btn-assinar">Experimente grátis <i class="fas fa-arrow-right"></i></button>
     </div>
   );
 }
